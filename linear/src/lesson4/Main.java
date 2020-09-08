c176ffc2646db3c4e5ba045c34d6d1ff1369f335
Vasko <maria.vasko911@gmail.com>
2020-09-08 15:47:22 +0200

    my second program

---
+++ linear/src/lesson4/Main.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 6;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+
+    }
+}
